7e84b12a646f033e7e43574b78287791ffeb7425
Tim Jones <nutpeg@gmail.com>
2017-07-26 15:56:11 +0200

    refactor: Book to functional comp. Tidy BookList.

--- src/Book.js
+++ src/Book.js
@@ -1,52 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Book extends Component {
-  // static defaultProps = {
-  //   authors: ["Unknown"],
-  //   imageLink: "none"
-  // };
-
-  render() {
-    const { book, handleShelfChange } = this.props;
-    let imageUrl;
-    if (book.imageLinks) {
-      imageUrl = `url("${book.imageLinks.thumbnail}")`;
-    } else {
-      imageUrl = 'none';
-    }
-    return (
-        <div className="book">
-          <div className="book-top">
-            <div
-                className="book-cover"
-                alt={`Cover image for ${book.title}`}
-                style={{
-                  width: 128,
-                  height: 193,
-                  backgroundImage: imageUrl
-                }}
-            />
-            <div className="book-shelf-changer">
-              <select value={book.shelf} onChange={(e) => handleShelfChange(e, book)}>
-                <option value="none" disabled>
-                  Move to...
-                </option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-          <div className="book-title">
-            {book.title}
-          </div>
-          <div className="book-authors">
-            {book.authors? book.authors.join(", ") : "Unknown"}
-          </div>
+const Book = ({ book, handleShelfChange }) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          alt={`Cover image for ${book.title}`}
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: book.imageLinks
+              ? `url("${book.imageLinks.thumbnail}")`
+              : "none"
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select value={book.shelf} onChange={e => handleShelfChange(e, book)}>
+            <option value="none" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-    );
-  }
-}
+      </div>
+      <div className="book-title">
+        {book.title}
+      </div>
+      <div className="book-authors">
+        {book.authors ? book.authors.join(", ") : "Unknown"}
+      </div>
+    </div>
+  );
+};
 
 export default Book;
--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
 import BooksGrid from "./BooksGrid";
 
@@ -34,45 +34,47 @@ class BookList extends Component {
   render() {
     const { allBooks } = this.state;
     return (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
-                <div className="bookshelf-books">
-                  <BooksGrid
-                      books={allBooks.filter(book => book.shelf === "currentlyReading")}
-                      handleShelfChange={this.handleShelfChange}
-                  />
-                </div>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <BooksGrid
+                  books={allBooks.filter(
+                    book => book.shelf === "currentlyReading"
+                  )}
+                  handleShelfChange={this.handleShelfChange}
+                />
               </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Want to Read</h2>
-                <div className="bookshelf-books">
-                  <BooksGrid
-                      books={allBooks.filter(book => book.shelf === "wantToRead")}
-                      handleShelfChange={this.handleShelfChange}
-                  />
-                </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <BooksGrid
+                  books={allBooks.filter(book => book.shelf === "wantToRead")}
+                  handleShelfChange={this.handleShelfChange}
+                />
               </div>
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">Read</h2>
-                <div className="bookshelf-books">
-                  <BooksGrid
-                      books={allBooks.filter(book => book.shelf === "read")}
-                      handleShelfChange={this.handleShelfChange}
-                  />
-                </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <BooksGrid
+                  books={allBooks.filter(book => book.shelf === "read")}
+                  handleShelfChange={this.handleShelfChange}
+                />
               </div>
             </div>
           </div>
-          <div className="open-search">
-            <Link to="/search">Add a book</Link>
-          </div>
         </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
     );
   }
 }
