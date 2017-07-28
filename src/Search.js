fc83f0287776399db6fcae0394e843db57604801
Tim Jones <nutpeg@gmail.com>
2017-07-29 00:49:44 +0200

    feat: Search on each additional character.

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import * as BooksAPI from "./BooksAPI";
-import serializeForm from "form-serialize";
 import BooksGrid from "./BooksGrid";
 import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     searchTerm: "",
-    searchResults: []
+    searchResults: [],
+    showSearchError: false
   };
 
   static propTypes = {
@@ -34,24 +34,57 @@ class Search extends Component {
     });
   };
 
-  handleSearchTermChange = event => {
-    let searchTerm = event.target.value;
-    this.setState({ searchTerm: searchTerm.trim() });
+  onSearchTermChange = searchTerm => {
+    this.setState({
+      showSearchError: false,
+      searchTerm: searchTerm.trim()
+    });
+    BooksAPI.search(searchTerm, 20)
+      .then(searchResults => {
+        let books = this.removeDuplicates(searchResults);
+        books = this.normalizeShelves(books);
+        this.setState(state => ({ searchResults: books }));
+      })
+      .catch(error => {
+        console.log(error.message);
+        let showError = searchTerm !== "";
+        // Seems that a previous search API call will sometimes
+        // render its results AFTER the following empty results
+        // have rendered. Happens when search terms are deleted
+        // quickly via backspace. Not sure how to fix this
+        // without making promise from API call cancelable.
+        // Is there a simpler solution?
+        this.setState(state => ({
+          showSearchError: showError,
+          searchResults: []
+        }));
+      });
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    const values = serializeForm(event.target, { hash: true });
-    BooksAPI.search(values.searchTerm, 1).then(searchResults => {
-      let books = this.removeDuplicates(searchResults);
-      books = this.normalizeShelves(books);
-      this.setState({ searchResults: books });
-    });
   };
 
   render() {
     const { searchTerm, searchResults } = this.state;
     const { handleShelfChange } = this.props;
+    let resultsPane;
+    if (this.state.showSearchError) {
+      resultsPane = (
+        <div>
+          <h2>No search results</h2>
+          <p>Please try another search term</p>
+        </div>
+      );
+    } else {
+      resultsPane = (
+        <BooksGrid
+          books={searchResults}
+          handleShelfChange={handleShelfChange}
+        />
+      );
+    }
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -59,23 +92,20 @@ class Search extends Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={event => this.handleSubmit(event)}>
               <input
                 type="text"
                 name="searchTerm"
                 placeholder="Search by title or author"
                 value={searchTerm}
-                onChange={this.handleSearchTermChange}
+                onChange={event => this.onSearchTermChange(event.target.value)}
                 autoFocus
               />
             </form>
           </div>
         </div>
         <div className="search-books-results">
-          <BooksGrid
-            books={searchResults}
-            handleShelfChange={handleShelfChange}
-          />
+          {resultsPane}
         </div>
       </div>
     );
